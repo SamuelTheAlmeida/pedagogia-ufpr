d7eeff10ff1387bc43eb3d5fb9cbba4173c3bae6
Samuel <samuel.t.almeida@gmail.com>
2019-05-25 16:48:50 -0300

    inclusao do import da navbar na index

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include("header.php"); ?>
+<?php include("navbars.php"); ?>
 
 							<p>O Trabalho de Conclusão de Curso (TCC) integra a formação do Pedagogo do Setor de Educação da Universidade Federal do Paraná, por isto, a sua elaboração é condição para a conclusão do aluno no curso. 
          O tema do TCC deverá se relacionar com a educação e suas interfaces com áreas afins, de modo a contribuir para a reflexão teórica, o desenvolvimento de práticas e metodologias de ensino, a análise de intervenções, a formulação e implementação de políticas públicas na educação.
