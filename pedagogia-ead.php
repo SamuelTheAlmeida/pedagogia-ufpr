fef3efe041c69fcda0947a8ca390aeb574a0d0a4
Samuel <samuel.t.almeida@gmail.com>
2019-05-25 17:34:19 -0300

    pagina pedagogia ead

---
+++ pedagogia-ead.php
@@ -0,0 +1,24 @@
+<?php $tituloPagina = "Pedagogia EaD"?>
+<?php include("header.php"); ?>
+<!-- Faça o CSS em um arquivo separado da pagina e importe aqui, antes de fechar o /head, exemplo abaixo -->
+<!-- <link rel="stylesheet" type="text/css" href="css/css-dessa-pagina.css"> -->
+</head>
+<?php include("navbars.php"); ?>
+
+							<p><em> O Curso de Licenciatura Plena em Pedagogia EaD – Magistério da Educação Infantil e Anos Iniciais do Ensino Fundamental abre vestibular para atender Polos em cidades conveniadas.</em></p>
+<p>O Curso de Pedagogia a distância normalmente funciona presencialmente aos sábados e o restante da carga horária será à distância por ambiente virtual de aprendizagem. A plataforma Moodle ficará disponibilizada vinte quatro horas para o acesso dos alunos, tutores presenciais e a distância, coordenação de polo e pedagógica da UFPR.</p>
+
+    <p class="bg-light text-info">     Informamos que a PREVISÃO para abertura de um novo edital de vestibular será para o primeiro semestre de 2019, com o possível ingresso para o segundo semestre de 2019.</p>
+
+<strong>Informações</strong>
+<address class="my-1">
+	<ul class="list-unstyled">
+		<li><strong>Fone:</strong> 3535-6252</li>
+		<li><strong>E-mail:</strong> coordenacaopedagogiaead@gmail.com</li>
+		<li><strong>Site:</strong> <a href="http://www.educacao.ufpr.br/portal/pedagogia-ead">http://www.educacao.ufpr.br/portal/pedagogia-ead</a></li>
+	</ul>
+
+
+
+</address>
+<?php include("footer.php"); ?>
