83aca51f7fc20a11d735ab975f6df9c2fbeb70ca
Gustavokovalski <gustavokovalski.saporiti@gmail.com>
2019-05-25 22:55:25 -0300

    criação de dropdown no btn secretaria(navbar) e tela processo-seletivo

---
+++ processo-seletivo.php
@@ -0,0 +1,25 @@
+<?php $tituloPagina = "Processo Seletivo"?>
+<?php include("header.php"); ?>
+<!-- Faça o CSS em um arquivo separado da pagina e importe aqui, antes de fechar o /head, exemplo abaixo -->
+<link rel="stylesheet" type="text/css" href="css/processo-seletivo.css">
+</head>
+<?php include("navbars.php"); ?>
+	
+	<h1>Processo Seletivo</h1>
+	<br/>
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-lg-10">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<br/>
+					<p><a href="http://portal.nc.ufpr.br/PortalNC/Home">Vestibular</a></p>
+					<p><a href="http://sisu.mec.gov.br/inicial">SISU</a></p>
+					<p><a href="http://www.prograd.ufpr.br/portal/provar/">PROVAR</a></p>
+				</li>
+			</ul>
+		</div>
+		<div class="col-md-1"></div>
+	</div>
+
+<?php include("footer.php"); ?>
