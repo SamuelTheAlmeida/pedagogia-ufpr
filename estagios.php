9fa4c3112b379bddacf13c5ccef0b40788edcab2
Gustavokovalski <gustavokovalski.saporiti@gmail.com>
2019-05-25 21:58:07 -0300

    criação tela de estágios (estagios.php)

---
+++ estagios.php
@@ -0,0 +1,50 @@
+<?php $tituloPagina = "Estágios"?>
+<?php include("header.php"); ?>
+<!-- Faça o CSS em um arquivo separado da pagina e importe aqui, antes de fechar o /head, exemplo abaixo -->
+<link rel="stylesheet" type="text/css" href="css/estagios.css">
+</head>
+<?php include("navbars.php"); ?>
+	<div class="row menu-estagio">
+		<div class="col-lg-4">
+			<ul class="list-group">
+				<li class="list-group-item"><a href="https://drive.google.com/file/d/1CIJnzRNMvK2s0ycI6OSjRJY3wwvVEn7h/view">Manual de Estágios</a></li>
+			</ul>
+		</div>
+		<div class="col-lg-4">
+			<ul class="list-group">
+				<li class="list-group-item"><a href="https://drive.google.com/file/d/1kxeFrkncG7LDFTFP6yqwqoUmGgCFpMZG/view">Reunião</a></li>
+			</ul>
+		</div>
+		<div class="col-lg-4">
+			<ul class="list-group">
+				<li class="list-group-item"><a href="http://200.17.193.102/tela_estagio_formularios/">Formulários</a></li>
+			</ul>
+		</div>
+			
+	</div>
+	<br/>
+
+	<h1>Unidade de estágios - COAFE</h1>
+	<br/>
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-lg-10">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<p>Endereço: Praça Santos Andrade, nº 50 – Prédio Histórico</p>
+
+					<p>Telefones: 3310-2627 / 3310-2656 / 3310-2675 / 3310-2681</p>
+					<p>E-mail: estagio@ufpr.br</p>
+
+					<p>Horário de atendimento:</p>
+
+					<p>Segunda à Sexta-feira das 08h00 às 12h00 e das 13h00 às 17h00</p>
+
+					<p>Para mais informações acesse <a href="http://www.prograd.ufpr.br/portal/coafe/">aqui</a></p>
+				</li>
+			</ul>
+		</div>
+		<div class="col-md-1"></div>
+	</div>
+
+<?php include("footer.php"); ?>
