b1e193b36e130f8ea0f3aa2c89950aa804451319
Gustavokovalski <gustavokovalski.saporiti@gmail.com>
2019-05-26 00:13:56 -0300

    Criação tela de atividades formativas.

---
+++ atividades-formativas.php
@@ -0,0 +1,31 @@
+<?php $tituloPagina = "Atividades Formativas"?>
+<?php include("header.php"); ?>
+<!-- Faça o CSS em um arquivo separado da pagina e importe aqui, antes de fechar o /head, exemplo abaixo -->
+<link rel="stylesheet" type="text/css" href="css/atividades-formativas.css"> 
+</head>
+<?php include("navbars.php"); ?>
+
+<h1>Consulta de Horas Formativas</h1>
+<p>Acesse as planilhas em PDF com a quantidade de horas computadas para cada aluno nos links abaixo, organizados por GRR:</p>
+<div class="row">
+<div class="col-lg-3"></div>
+	<div class="col-lg-6">
+		<ul class="list-group">
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/14RS1S5Y5VxTXRmgBH_NYQ-5uKIw32YhV/view" target="_blanked">2018</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/0Bzs3uTGDWGhIaENwWHBSdHg0SXc/view" target="_blanked">2017</a></li>
+			<li class="list-group-item"><a  href="https://drive.google.com/file/d/0Bzs3uTGDWGhIamZmc1NjZHYyNWc/view" target="_blanked">2016</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/1sYjz4i7taUuAZbfgg4HxU6MpLc_tyvfk/view" target="_blanked">2015</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/12sGNtA9bPTEhouCuw6YIgXWROWJChpPC/view" target="_blanked">2014</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/1S49pG_BS86hIh352S4VJp8H1A3lU2fDZ/view" target="_blanked">2013</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/1XhyCnSuUAqbp1cN6pN313w4Wy0uwz1a2/view" target="_blanked">2012</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/15-1_TUZonsnEu3yr7jeQa7aSkeFfL4ZJ/view" target="_blanked">2011</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/1k5x1xbvOBsizLnzQBEJSHvJmVhx8oSPr/view" target="_blanked">2010</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/0Bzs3uTGDWGhIN2YxZnpaMXZHOE0/view" target="_blanked">2009</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/15HCgixoBHoYsJ3U6UoVbCczbHUeLW5xs/view" target="_blanked">2002-03</a></li>
+			<li class="list-group-item"><a href="https://drive.google.com/file/d/0Bzs3uTGDWGhIVlJTYkIzUWo3VDQ/view" target="_blanked">Outros anos</a></li>
+		</ul>
+	</div>
+<div class="col-lg-3"></div>
+</div>
+
+<?php include("footer.php"); ?>
