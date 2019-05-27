61d3a1390174a2805427e3782a7854894bbc59cb
Samuel <samuel.t.almeida@gmail.com>
2019-05-27 14:06:55 -0300

    jquery masks

--- header.php
+++ header.php
@@ -22,4 +22,4 @@
 <!-- include the calendar js and css files -->
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/zabuto_calendar/1.6.4/zabuto_calendar.css">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script type="text/javascript" src="jquery.mask.min.js"></script>
+<script type="text/javascript" src="jquery.mask.js"></script>
