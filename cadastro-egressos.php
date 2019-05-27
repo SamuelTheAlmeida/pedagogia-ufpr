065a5e1dd783ffbb0d3b0f2324d5f9085a27c9a6
Samuel <samuel.t.almeida@gmail.com>
2019-05-27 14:12:51 -0300

    jquery masks

--- cadastro-egressos.php
+++ cadastro-egressos.php
@@ -100,7 +100,7 @@
 </form>
 <script>
   $(document).ready(function(){
-    $('#inputNome').mask('[a-z]');
+    $('#inputNome').mask('a-z');
     $('#inputCPF').mask('000.000.000-00');
     $('#inputData').mask('00/00/0000');
     $('#inputTelefone').mask('(00) 00000-0000');
